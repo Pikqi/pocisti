74d9e3aed314d1beb99fc3dbce4cbc52df40855c
Petar Obradovic <perazmn@gmail.com>
2022-01-26 22:46:15 +0100

    added scrollable card view

--- components/Map.js
+++ components/Map.js
@@ -1,4 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Animated,
+  Dimensions,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React, { useState, useEffect } from "react";
 import MapView, { Marker } from "react-native-maps";
 import { Button } from "react-native-elements";
@@ -7,12 +15,45 @@ import { addDoc, collection, doc, getDoc, getDocs, Timestamp } from "firebase/fi
 import * as Location from "expo-location";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
 
+const { width, height } = Dimensions.get("window");
+const CARD_HEIGHT = 220;
+const CARD_WIDTH = width * 0.8;
+const SPACING_FOR_CARD_INSET = width * 0.1 - 10;
+
 const Map = () => {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   const [markers, setMarkers] = useState([]);
 
+  let mapAnimation = new Animated.Value(0);
+
+  const _scrollView = React.useRef(null);
+
   const ANCHOR = { x: 0.5, y: 0.5 };
+
+  // useEffect(() => {
+  //   getUserLocation();
+  // }, []);
+
+  // const getUserLocation = async () => {
+  //   let { status } = await Location.getForegroundPermissionsAsync();
+  //   if (status !== "granted") {
+  //     setErrorMsg("Permission to access location was denied");
+  //     return;
+  //   }
+
+  //   await Location.enableNetworkProviderAsync();
+
+  //   let unsub = await Location.watchPositionAsync(
+  //     { accuracy: Location.Accuracy.High, timeInterval: 500 },
+  //     (loc) => {
+  //       console.log(loc);
+  //       setLocation(loc);
+  //     }
+  //   );
+
+  //   return unsub;
+  // };
 
   useFocusEffect(
     React.useCallback(async () => {
@@ -44,53 +85,28 @@ const Map = () => {
         id: item.id,
         longitude: item.data().longitude,
         latitude: item.data().latitude,
+        adress: item.data().adress,
+        description: item.data().description,
+        imageLink: item.data().imageLink,
+        userId: item.data().userId,
+        userName: item.data().userName,
       }));
       setMarkers(m);
       return () => querySnapshot();
     }, [])
   );
-  // useEffect(() => {
-  //   getCurrentLocation();
-  //   getMarkers();
-  // }, [refresh]);
 
-  // useFocusEffect(() => {
-  //   getCurrentLocation();
-  // });
+  const interpolations = markers.map((marker, index) => {
+    const inputRange = [(index - 1) * CARD_WIDTH, index * CARD_WIDTH, (index + 1) * CARD_WIDTH];
 
-  // const getMarkers = async () => {
-  //   const querySnapshot = await getDocs(collection(db, "depo"));
+    const scale = mapAnimation.interpolate({
+      inputRange,
+      outputRange: [1, 1.5, 1],
+      extrapolate: "clamp",
+    });
 
-  //   const m = querySnapshot.docs.map((item) => ({
-  //     id: item.id,
-  //     longitude: item.data().longitude,
-  //     latitude: item.data().latitude,
-  //   }));
-  //   setMarkers(m);
-  //   // console.log(markers);
-  //   // console.log(m);
-  // };
-  // getting permisssion for location and location obj from expo docs
-
-  // const getCurrentLocation = async () => {
-  //   let { status } = await Location.getForegroundPermissionsAsync();
-  //   if (status !== "granted") {
-  //     setErrorMsg("Permission to access location was denied");
-  //     return;
-  //   }
-
-  //   await Location.enableNetworkProviderAsync();
-
-  //   let unsub = await Location.watchPositionAsync(
-  //     { accuracy: Location.Accuracy.High, timeInterval: 500 },
-  //     (loc) => {
-  //       console.log(loc);
-  //       setLocation(loc);
-  //     }
-  //   );
-
-  //   return unsub;
-  // };
+    return { scale };
+  });
 
   return (
     <View style={{ height: "100%" }}>
@@ -104,19 +120,7 @@ const Map = () => {
           longitudeDelta: 0.2,
         }}
       >
-        {markers.map((marker) => (
-          <Marker
-            key={marker.id}
-            anchor={ANCHOR}
-            style={styles.mapMarker}
-            coordinate={{
-              latitude: marker.latitude,
-              longitude: marker.longitude,
-            }}
-          ></Marker>
-        ))}
-
-        {/* marker trenutne lokacije direkt kopirano sa githuba */}
+        {/* Marker of current user location */}
         {location && (
           <Marker
             key={1}
@@ -133,7 +137,101 @@ const Map = () => {
             </View>
           </Marker>
         )}
+
+        {/* depo markers */}
+        {markers.map((marker, index) => {
+          const scaleStyle = {
+            transform: [
+              {
+                scale: interpolations[index].scale,
+              },
+            ],
+          };
+          return (
+            <Marker
+              key={index}
+              coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+              onPress={(e) => onMarkerPress(e)}
+            >
+              <Animated.View style={[styles.markerWrap]}>
+                <Animated.Image
+                  source={require("../assets/trash.png")}
+                  style={[styles.markerIcon, scaleStyle]}
+                  resizeMode="cover"
+                />
+              </Animated.View>
+            </Marker>
+          );
+        })}
       </MapView>
+      <Animated.ScrollView
+        ref={_scrollView}
+        horizontal
+        pagingEnabled
+        scrollEventThrottle={1}
+        showsHorizontalScrollIndicator={false}
+        snapToInterval={CARD_WIDTH + 20}
+        snapToAlignment="center"
+        style={styles.scrollView}
+        contentInset={{
+          top: 0,
+          left: SPACING_FOR_CARD_INSET,
+          bottom: 0,
+          right: SPACING_FOR_CARD_INSET,
+        }}
+        contentContainerStyle={{
+          paddingHorizontal: Platform.OS === "android" ? SPACING_FOR_CARD_INSET : 0,
+        }}
+        onScroll={Animated.event(
+          [
+            {
+              nativeEvent: {
+                contentOffset: {
+                  x: mapAnimation,
+                },
+              },
+            },
+          ],
+          { useNativeDriver: true }
+        )}
+      >
+        {markers.map((marker) => (
+          <View style={styles.card} key={marker.id}>
+            <Image source={{ uri: marker.imageLink }} style={styles.cardImage} resizeMode="cover" />
+            <View style={styles.textContent}>
+              <Text numberOfLines={1} style={styles.cardtitle}>
+                {marker.adress}
+              </Text>
+              <Text numberOfLines={1} style={styles.cardDescription}>
+                {marker.description}
+              </Text>
+              <View style={styles.button}>
+                <TouchableOpacity
+                  onPress={() => {}}
+                  style={[
+                    styles.signIn,
+                    {
+                      borderColor: "green",
+                      borderWidth: 1,
+                    },
+                  ]}
+                >
+                  <Text
+                    style={[
+                      styles.textSign,
+                      {
+                        color: "green",
+                      },
+                    ]}
+                  >
+                    Otvori
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        ))}
+      </Animated.ScrollView>
     </View>
   );
 };
@@ -182,5 +280,78 @@ const styles = StyleSheet.create({
     height: SIZE,
     borderRadius: Math.ceil(SIZE / 2),
     margin: (HEADING_BOX_SIZE - SIZE) / 2,
+  },
+  scrollView: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    paddingVertical: 10,
+  },
+  endPadding: {
+    paddingRight: width - CARD_WIDTH,
+  },
+  card: {
+    // padding: 10,
+    elevation: 2,
+    backgroundColor: "#FFF",
+    borderTopLeftRadius: 5,
+    borderTopRightRadius: 5,
+    marginHorizontal: 10,
+    shadowColor: "#000",
+    shadowRadius: 5,
+    shadowOpacity: 0.3,
+    shadowOffset: { x: 2, y: -2 },
+    height: CARD_HEIGHT,
+    width: CARD_WIDTH,
+    overflow: "hidden",
+  },
+  cardImage: {
+    flex: 3,
+    width: "100%",
+    height: "100%",
+    alignSelf: "center",
+  },
+  textContent: {
+    flex: 2,
+    padding: 10,
+  },
+  cardtitle: {
+    fontSize: 12,
+    // marginTop: 5,
+    fontWeight: "bold",
+  },
+  cardDescription: {
+    fontSize: 12,
+    color: "#444",
+  },
+  markerWrap: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 50,
+    height: 50,
+  },
+  // marker: {
+  //   width: 30,
+  //   height: 30,
+  // },
+  button: {
+    alignItems: "center",
+    marginTop: 5,
+  },
+  signIn: {
+    width: "100%",
+    padding: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 3,
+  },
+  textSign: {
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  markerIcon: {
+    width: 30,
+    height: 30,
   },
 });

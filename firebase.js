6a76f7b684e182e164e6359585b7bb6a434f73b7
Petar Obradovic <perazmn@gmail.com>
2022-01-26 19:13:35 +0100

    changed firebase config, again...

--- firebase.js
+++ firebase.js
@@ -2,14 +2,13 @@ import { getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBvh5s3YFhL4EXTtGPbU8u48TFmAuhaw_c",
-  authDomain: "pocistiapp.firebaseapp.com",
-  projectId: "pocistiapp",
-  storageBucket: "pocistiapp.appspot.com",
-  messagingSenderId: "499556068359",
-  appId: "1:499556068359:web:74ea1923b1cbe954260ea2",
+  apiKey: "AIzaSyBwI6NxpsZCHUOchFT6gNfct_pZU_H1WdE",
+  authDomain: "signal-clone-c44c5.firebaseapp.com",
+  projectId: "signal-clone-c44c5",
+  storageBucket: "signal-clone-c44c5.appspot.com",
+  messagingSenderId: "747887420178",
+  appId: "1:747887420178:web:07796e367b7514a07689cb",
 };
-
 if (!getApps().length) {
   initializeApp(firebaseConfig);
 }
@@ -17,3 +16,4 @@ if (!getApps().length) {
 // const app = initializeApp(firebaseConfig);
 const db = getFirestore();
 export { db };
+``;

24eac31677b68576b33d424ac9e49806ae25d79f
Petar Obradovic <perazmn@gmail.com>
2022-01-27 22:21:19 +0100

    updated

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,12 @@
-import { StyleSheet, Text, View, KeyboardAvoidingView, Platform } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
@@ -29,36 +37,38 @@ const LoginScreen = () => {
   });
 
   return (
-    <KeyboardAvoidingView
-      style={styles.container}
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
-    >
-      <View style={styles.inputContainer}>
-        <Input
-          placeholder="E-mail"
-          type="email"
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-        ></Input>
-        <Input
-          placeholder="Sifra"
-          secureTextEntry
-          type="password"
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-          onSubmitEditing={signIn}
-        ></Input>
-      </View>
-      <Button containerStyle={styles.button} onPress={signIn} title="Login"></Button>
-      <Button
-        containerStyle={styles.button}
-        onPress={() => navigation.navigate("Register")}
-        type="outline"
-        title="Register"
-      ></Button>
-      <View style={{ height: 200 }}></View>
-    </KeyboardAvoidingView>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+      >
+        <View style={styles.inputContainer}>
+          <Input
+            placeholder="E-mail"
+            type="email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          ></Input>
+          <Input
+            placeholder="Sifra"
+            secureTextEntry
+            type="password"
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            onSubmitEditing={signIn}
+          ></Input>
+        </View>
+        <Button containerStyle={styles.button} onPress={signIn} title="Login"></Button>
+        <Button
+          containerStyle={styles.button}
+          onPress={() => navigation.navigate("Register")}
+          type="outline"
+          title="Register"
+        ></Button>
+        <View style={{ height: 200 }}></View>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 };
 
